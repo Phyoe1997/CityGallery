23e862b39b8f732b4b5ee36683e9f8301799f6e7
Phyo <39774502+Phyoe1997@users.noreply.github.com>
2018-11-12 16:25:37 +0900

    initial Commit

---
+++ app.js
@@ -0,0 +1,85 @@
+//the large image
+let largeImg = document.getElementById('current');
+let placeName = document.getElementById('placeName');
+
+// let img1 = document.getElementById('img1');
+// let img2= document.getElementById('img2');
+// let img3= document.getElementById('img3');
+// let img4 = document.getElementById('img4');
+// let img5 = document.getElementById('img5');
+// let img6 = document.getElementById('img6');
+// let img7 = document.getElementById('img7');
+// let img8 = document.getElementById('img8');
+
+// img1.addEventListener('click',function(event){
+//   largeImg.src = event.target.src
+//   placeName.innerHTML = event.target.alt
+// });
+
+// img2.addEventListener('click',function(event){
+//   largeImg.src = event.target.src
+//   placeName.innerHTML = event.target.alt
+// });
+
+// img3.addEventListener('click',function(event){
+//   largeImg.src = event.target.src
+//   placeName.innerHTML = event.target.alt
+// });
+
+// img4.addEventListener('click',function(event){
+//   largeImg.src = event.target.src
+//   placeName.innerHTML = event.target.alt
+// });
+
+// img5.addEventListener('click',function(event){
+//   largeImg.src = event.target.src
+//   placeName.innerHTML = event.target.alt
+// });
+
+// img6.addEventListener('click',function(event){
+//   largeImg.src = event.target.src
+//   placeName.innerHTML = event.target.alt
+// });
+
+// img7.addEventListener('click',function(event){
+//   largeImg.src = event.target.src
+//   placeName.innerHTML = event.target.alt
+// });
+
+// img8.addEventListener('click',function(event){
+//   largeImg.src = event.target.src
+//   placeName.innerHTML = event.target.alt
+// });
+
+
+
+
+
+//Create an array with all imags
+
+// const allImags = [img1,img2,img3,img4,img5,img6,img7,img8];
+// allImags[0].addEventListener('click',switchImg);
+// allImags[1].addEventListener('click',switchImg);
+// allImags[2].addEventListener('click',switchImg);
+// allImags[3].addEventListener('click',switchImg);
+// allImags[4].addEventListener('click',switchImg);
+// allImags[5].addEventListener('click',switchImg);
+// allImags[6].addEventListener('click',switchImg);
+// allImags[7].addEventListener('click',switchImg);
+
+
+// function switchImg(event){
+//   current.src = event.target.src;
+//   placeName.innerHTML = event.target.alt;
+// }
+
+
+const allImgsEasy = document.querySelectorAll('.imgs img');
+
+for(let counter = 0;counter <=7;
+counter = counter + 1){
+  allImgsEasy[counter].addEventListener('click',function(event){
+    current.src = event.target.src;
+    placeName.innerHTML = event.target.alt;
+  })
+}
